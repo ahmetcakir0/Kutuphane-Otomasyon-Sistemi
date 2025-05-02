036daf475ccaed24af230f92523f1087046114c8
Ahmet <166337069+ahmetcakir0@users.noreply.github.com>
2025-05-02 13:19:51 +0300

    Update KatRafDAL.cs

--- DataAccessLayer/KatRafDAL.cs
+++ DataAccessLayer/KatRafDAL.cs
@@ -8,7 +8,7 @@ namespace DataAccessLayer
 {
     public class KatRafDAL
     {
-        private readonly string connectionString = "server=MBB-01-BIL065-N\\SQLEXPRESS; Initial Catalog=KutuphaneDB; Integrated Security=SSPI";
+        private readonly string connectionString = "Kendi veri tabanınızı girin.(Ben MSSQL kullandım.)";
 
         public bool KategoriEkle(string kategoriAdi)
         {
