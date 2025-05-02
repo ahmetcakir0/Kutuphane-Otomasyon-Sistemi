323f089f2dbb0440e8523d9af53a9a7b4ca2bf79
Ahmet <166337069+ahmetcakir0@users.noreply.github.com>
2025-05-02 13:21:38 +0300

    Update YayineviDAL.cs

--- DataAccessLayer/YayineviDAL.cs
+++ DataAccessLayer/YayineviDAL.cs
@@ -8,7 +8,7 @@ namespace DataAccessLayer
 {
     public class YayineviDAL
     {
-        private readonly string connectionString = "server=MBB-01-BIL065-N\\SQLEXPRESS; Initial Catalog=KutuphaneDB; Integrated Security=SSPI";
+        private readonly string connectionString = "Kendi veri tabanınızı girin.(Ben MSSQL kullandım.)";
 
         public YayineviDAL() { }
 
