56db3f832490a1adfc521b284b801da30a921c47
Ahmet <166337069+ahmetcakir0@users.noreply.github.com>
2025-05-02 13:21:25 +0300

    Update UyeDAL.cs

--- DataAccessLayer/UyeDAL.cs
+++ DataAccessLayer/UyeDAL.cs
@@ -11,7 +11,7 @@ namespace DataAccessLayer
 {
     public class UyeDAL
     {
-        private readonly string connectionString = "server=MBB-01-BIL065-N\\SQLEXPRESS; Initial Catalog=KutuphaneDB; Integrated Security=SSPI";
+        private readonly string connectionString = "Kendi veri tabanınızı girin.(Ben MSSQL kullandım.)";
 
         public UyeDAL()
         {
